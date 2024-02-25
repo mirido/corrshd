bb128eda91b6ef1f2ad2f17f35993206696cec27
mirido <ic342117xwir@yahoo.co.jp>
2024-02-25 13:51:38 +0900

    Measures against false images caused by calculations in ImgFunc_uniform.
    Suppress the occurrence of false images due to approximate extrapolation
    by adding artificial blackness samples.

--- ShadhingCorrection/ImgFunc_uniform.cpp
+++ ShadhingCorrection/ImgFunc_uniform.cpp
@@ -22,6 +22,82 @@ const char* ImgFunc_uniform::getSummary() const
 	return "Uniform peak level of pixels.";
 }
 
+namespace
+{
+	/// Predict while image.
+	bool add_artificial_samples_to_modify_uncorrectables(
+		const cv::Size& imgSz,
+		const cv::Size& kernelSz,
+		const std::vector<double>& cflist,
+		std::vector<LumSample>& samplesOnDL
+	)
+	{
+		// Get mean and stddev.
+		const int n = C_INT(samplesOnDL.size());
+		cv::Mat data(cv::Size(n, 1), CV_32SC1);
+		for (int j = 0; j < n; j++) {
+			data.at<int>(0, j) = C_INT(samplesOnDL[j].m_lum);
+		}
+		const cv::Scalar mean, stddev;
+		cv::meanStdDev(data, mean, stddev);
+		const double fmean = mean[0];
+		const double fstddev = stddev[0];
+		auto sv_flags = cout.flags();
+		cout << "  mean=" << std::showpos << fmean << endl;
+		cout << "stddev=" << std::showpos << fstddev << endl;
+		cout.flags(sv_flags);
+
+		// Decide minimum value of black level.
+		//const double minBlackLevel = 0.0;
+		//const double minBlackLevel = std::max(0.0, fmean);
+		const double minBlackLevel = std::max(0.0, fmean - fstddev * 0.7);
+		//const double minBlackLevel = std::max(0.0, fmean - fstddev);
+		cout << "minBlackLevel=" << minBlackLevel << endl;
+
+		auto addSmpIfFunc = [=, &cflist, &samplesOnDL](const cv::Point& pt) {
+			double fLum = predict_by_qubic_poly(cflist, C_DBL(pt.x), C_DBL(pt.y));
+
+			// For fLum, 0 means white, 255 means black.
+			// The fLum on (x, y) will be stretched in propotion to ratio of [0, 255] to [0, flum]. 
+			// Therefore, if fLum is too small, the value cannot be determined.
+			if (fLum <= C_DBL(minBlackLevel)) {
+				LumSample val;
+				val.m_point = pt;
+				val.m_lum = C_UCHAR(minBlackLevel);
+				samplesOnDL.push_back(val);
+				return true;
+			}
+			return false;
+		};
+
+		const int sx = kernelSz.width / 2;
+		const int sy = kernelSz.height / 2;
+		const int cyc_x = kernelSz.width;
+		const int cyc_y = kernelSz.height;
+
+		bool bChanged = false;
+		for (int y = sy; y < imgSz.height; y += cyc_y) {
+			if (addSmpIfFunc(cv::Point(0, y))) {
+				bChanged = true;
+			}
+			if (addSmpIfFunc(cv::Point(imgSz.width - 1, y))) {
+				bChanged = true;
+			}
+		}
+		for (int x = sx; x < imgSz.width; x += cyc_x) {
+			if (addSmpIfFunc(cv::Point(x, 0))) {
+				bChanged = true;
+			}
+			if (addSmpIfFunc(cv::Point(x, imgSz.height - 1))) {
+				bChanged = true;
+			}
+		}
+
+		return bChanged;
+	}
+
+}	// namespace
+
 bool ImgFunc_uniform::run(const cv::Mat& srcImg, cv::Mat& dstImg)
 {
 	// This method assumes that background pixels of srcImg are almost equalized to 0.
@@ -37,19 +113,32 @@ bool ImgFunc_uniform::run(const cv::Mat& srcImg, cv::Mat& dstImg)
 	cv::Mat morphoTmpImg;
 	cv::dilate(srcImg, morphoTmpImg, kernel);
 	auto samplesOnDL = sampleDrawLine(morphoTmpImg, maskToKeepDL);
-#ifndef NDEBUG
-	cout << "samplesOnDL: size=" << samplesOnDL.size() << endl;
-	plotSamples(morphoTmpImg, samplesOnDL, "samples on drawing line");
-#endif
-	morphoTmpImg.release();
 
 	maskToKeepDL.release();
 
-	// Approximage blackness tilt on drawing line.
+	// Approximage blackness tilt on drawing line (1st).
 	std::vector<double> cflistOnDL;
 	if (!approximate_lighting_tilt_by_cubic_poly(samplesOnDL, cflistOnDL)) {
 		return false;
 	}
+
+	// Check uncorrectable pixels exist or not.
+	const size_t sv_nsamples = samplesOnDL.size();
+	if (add_artificial_samples_to_modify_uncorrectables(srcImg.size(), kernel.size(), cflistOnDL, samplesOnDL)) {
+		cout << "nsamples on drawing line changed. (" << sv_nsamples << " --> " << samplesOnDL.size() << ")" << endl;
+
+		// Approximage blackness tilt on drawing line (2nd).
+		if (!approximate_lighting_tilt_by_cubic_poly(samplesOnDL, cflistOnDL)) {
+			return false;
+		}
+	}
+	else {
+		assert(samplesOnDL.size() == sv_nsamples);
+	}
+
+	cout << "samplesOnDL: size=" << samplesOnDL.size() << endl;
+	plotSamples(morphoTmpImg, samplesOnDL, "samples on drawing line");
+	morphoTmpImg.release();
 
 	// Enhance drawing line.
 	cv::Mat invBlacknessTiltImg;
